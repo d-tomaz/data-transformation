e13a59d9edf096d2d5c0651f6b32997ea77cb2b8
Daniel Tomaz <49313643+d-tomaz@users.noreply.github.com>
2023-05-23 12:50:48 -0300

    Create function to replace data in desired column

--- pdf_table_extraction.py
+++ pdf_table_extraction.py
@@ -5,3 +5,18 @@ import zipfile
 
 pdf_path = "Anexo_I.pdf"
 csv_path = "Anexo_I.csv"
+
+def replace_csv_data(csv_path, column, replacement):
+    temporary_name = "temp.csv"
+    with open(csv_path, "r") as csv_file, open(temporary_name, "w", newline = "") as temporary_file:
+        csv_reader = csv.DictReader(csv_file)
+        header = csv_reader.fieldnames
+
+        csv_writer = csv.DictWriter(temporary_file, fieldnames = header)
+        csv_writer.writeheader()
+
+        for row in csv_reader:
+            row[column] = replacement
+            csv_writer.writerow(row)
+
+    os.replace(temporary_name, csv_path)
