23bbb16a9a71a7422d96528063b1283f1ff7a168
Daniel Tomaz <49313643+d-tomaz@users.noreply.github.com>
2023-05-23 13:49:53 -0300

    Edit pdf_table_extraction.py

--- pdf_table_extraction.py
+++ pdf_table_extraction.py
@@ -7,11 +7,11 @@ pdf_path = "Anexo_I.pdf"
 csv_path = "Anexo_I.csv"
 
 def replace_csv_data(csv_path, column, replacement):
-    temporary_name = 'temp.csv'
-    with open(csv_path, 'r') as file, open(temporary_name, 'w', newline='') as temp_file:
+    temporary_name = "temporary.csv"
+    with open(csv_path, "r") as file, open(temporary_name, "w", newline = "") as temporary_file:
         reader = csv.reader(file)
-        writer = csv.writer(temp_file)
-        
+        writer = csv.writer(temporary_file)
+
         for row in reader:
             updated_row = [cell.replace(column, replacement) if cell == column else cell for cell in row]
             writer.writerow(updated_row)
